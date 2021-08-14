17a99cb2522fbc50a80f8724db61f4bd5a4edad3
Nizami Islamovs <nizamiislamovs@gmail.com>
2021-08-14 23:43:39 +0300

    Added twitter consumer draft.

---
+++ twitterConsumer/src/test/java/com/example/twitterConsumer/TwitterConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.twitterConsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
